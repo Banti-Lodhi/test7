7f4266bbaeaed91bf918b631225f271c7be7dd29
banti64963@gmail.com <banti64963@gmail.com>
2025-03-08 11:36:16 +0530

    added

---
+++ GitProjectComApplication.java
@@ -0,0 +1,13 @@
+package com.GitComProject.GitProjectCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProjectComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProjectComApplication.class, args);
+	}
+
+}
